1d4a6e356cf6ea4ba7e9df9c22e51a9ee71024fd
shaun-sweet <shaun@shaunsweet.com>
2017-01-19 18:08:12 -0700

    basic api call function skeleton setup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-  $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?', function(data) {
-    console.log(data);
-  });
+  getChannelData("freecodecamp");
+  getStreamData("freecodecamp");
+  getUserData("freecodecamp");
 });
-// https://api.twitch.tv/kraken/channels/freecodecamp
+
+function getChannelData(user) {
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+user+'?callback=?', function(data) {
+    console.log("Channel Data: ", data);
+  });
+}
+
+function getStreamData(user) {
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+user+'?callback=?', function(data) {
+    console.log("Stream Data: ", data);
+  });
+}
+
+function getUserData(user) {
+  $.getJSON('https://wind-bow.gomix.me/twitch-api/users/'+user+'?callback=?', function(data) {
+    console.log("User Data: ", data);
+  });
+}
